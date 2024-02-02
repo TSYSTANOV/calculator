0767929650a05491e62cd375e7dcd81e89f00438
TSYSTANOV <tsystanov.vitaly@gmail.com>
2024-02-02 15:42:48 +0200

    create main functions

--- Desktop/testing/index.js
+++ Desktop/testing/index.js
@@ -1,37 +1,78 @@
-let display = document.querySelector(".display");
-let calc = ["+", "-", "/", "*"];
-let res = "";
-let prevKey = null;
+let display = document.querySelector(".display > span");
+let result = document.querySelector(".display > .result");
+let operators = ["+", "-", "/", "*",'.'];
+let arr = []
+
 let btns = document.querySelectorAll("button");
 btns.forEach((el) => {
-  el.addEventListener("click", () => {
-    display.textContent += el.textContent;
-    // if (el.className === "deleteAll") {
-    //   display.textContent = "";
-    //   return;
-    // }
-    // if (el.className === "result") {
-    // }
-    // if (calc.indexOf(`${el.innerHTML}`) === -1) {
-    //   res += el.innerHTML;
-    // }
+  el.addEventListener('click',()=>
+  {
 
-    if (calc.indexOf(`${el.innerHTML}`) === 0) {
-      prevKey = `${el.innerHTML}`;
-      return;
+    let btnValue = el.innerHTML;    
+
+    if(btnValue === 'C')
+    {
+      arr = []
+      display.innerHTML = '';
+      result.innerHTML = '';
+      return
     }
-
-    if (prevKey === "+") {
-      res = +res + +el.innerHTML;
-      prevKey = "";
-      console.log(res);
-    } else {
-      res += el.innerHTML;
+    else if(btnValue === '=')
+    {
+      if(display.innerHTML.includes('%'))
+      {
+        arr.splice(arr.indexOf('%'),1,'/100') 
+        console.log(arr)
+      }
+      display.innerHTML = arr.join('')
+     try
+     {
+      result.innerHTML = `= ${eval(display.innerHTML)}`
+     }catch(error){
+      result.innerHTML = '= ERROR'
+     }
+      
+      return
     }
+      else if(btnValue === 'arrow')
+      {
+      arr.pop()
+      display.innerHTML = arr.join('')
 
-    // if (el.className === "plus") {
-    //   console.log(parseFloat(display.textContent));
-    // }
-    // display.textContent += el.textContent;
-  });
+      if(arr.length === 0)
+      {
+        display.innerHTML = ''
+        result.innerHTML = ''
+      }
+      }
+    else if(operators.indexOf(btnValue) > -1)
+    {
+      
+      if(display.innerHTML[display.innerHTML.length-1] === btnValue)
+      {
+        return
+      }
+      
+      for(let i = 0; i < operators.length; i++)
+      {
+        let item = operators[i]
+     
+        if(item === display.innerHTML[display.innerHTML.length-1])
+        {
+          arr.pop()
+          arr.push(btnValue)
+          
+          display.innerHTML = arr.join('')
+          return
+        }
+      }
+      display.textContent += btnValue
+      arr.push(btnValue)
+    }
+    else{
+      display.innerHTML += btnValue
+      arr.push(btnValue)
+   
+    }
+  })
 });
